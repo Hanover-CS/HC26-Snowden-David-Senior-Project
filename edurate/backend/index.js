476200a42ecad8a424f71ebe9fbb67090e5d46ac
David Snowden <143359965+davidsnowden0072@users.noreply.github.com>
2025-10-14 17:21:38 +0000

    Completed Backend setup and testing I also added connection from my backend index.js file to supabase and imported nessicary testing libraries

--- edurate/backend/index.js
+++ edurate/backend/index.js
@@ -8,4 +8,15 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
+}); 
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://lhhaskcrrtaquzcddxpd.supabase.com";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoaGFza2NycnRhcXV6Y2RkeHBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MzA4MzMsImV4cCI6MjA3NjAwNjgzM30.UVf6sMs05MEcKvYs-P4YAPtsNKifX4NaoSsg6cTJrFc";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+app.get("/test-db", async (req, res) => {
+  const { data, error } = await supabase.from("courses").select("*");
+  res.json({ data, error });
 });
